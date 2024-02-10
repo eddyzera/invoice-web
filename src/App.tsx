481b8b96b6a99ddfa0223314b9851bcf32e0f234
eddyzera <esilva644@gmail.com>
2024-02-10 16:59:16 -0300

    style: using shadcn-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
 
+import { Button } from './components/ui/button'
+
 export const App: React.FunctionComponent = () => {
-  return <div>hello</div>
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <Button>Enviar</Button>
+    </div>
+  )
 }
