c193e52e7011569ac99edbf707e20794972a138d
eddyzera <esilva644@gmail.com>
2024-02-11 14:18:16 -0300

    feat: creating styles to status

--- src/components/invoice/index.tsx
+++ src/components/invoice/index.tsx
@@ -1,22 +1,38 @@
 import { ChevronRight } from 'lucide-react'
 import React from 'react'
 
-export const Invoice: React.FunctionComponent = () => {
+type Status = 'paid' | 'pending' | 'draft' | 'late'
+
+interface IInvoiceProps {
+  id: string
+  date: string
+  customer: string
+  price: string
+  status: Status
+}
+
+export const Invoice: React.FunctionComponent<IInvoiceProps> = ({
+  id,
+  date,
+  customer,
+  price,
+  status
+}) => {
   return (
     <div className='grid grid-cols-2 p-6 border w-full rounded-md justify-between cursor-pointer'>
       <div className='flex items-center justify-between'>
         <p className='font-bold text-stone-400'>
           <span className='text-stone-600'>#</span>
-          RT3080
+          {id}
         </p>
-        <p className='text-muted-foreground'>Due  19 Aug 2021</p>
-        <p className='text-muted-foreground'>Jensen Huang</p>
+        <p className='text-muted-foreground'>{date}</p>
+        <p className='text-muted-foreground'>{customer}</p>
       </div>
       <div className='flex items-center justify-end gap-5'>
-        <p className='text-muted-foreground font-bold'>$ 1,800.90</p>
-        <div className='flex items-center justify-center gap-2 p-3 bg-emerald-100 rounded-md w-24'>
-          <div className='w-2 h-2 bg-emerald-600 rounded-full'></div>
-          <p className='text-emerald-600'>Paid</p>
+        <p className='text-muted-foreground font-bold'>{price}</p>
+        <div className={`flex items-center justify-center gap-2 p-3 ${status} rounded-md w-24`}>
+          <span className='w-2 h-2 block rounded-full'></span>
+          <p className='capitalize'>{status}</p>
         </div>
         <ChevronRight />
       </div>
--- src/pages/app/dashboard/index.tsx
+++ src/pages/app/dashboard/index.tsx
@@ -4,9 +4,10 @@ import React from 'react'
 export const Dashboard: React.FunctionComponent = () => {
   return (
     <div className='flex items-center flex-col gap-4 justify-center pt-16 max-w-4xl m-auto'>
-      <Invoice />
-      <Invoice />
-      <Invoice />
+      <Invoice id='RT3080' date='Due  19 Aug 2021' customer='Jensen Huang' price='£ 1,800.90' status={'paid'} />
+      <Invoice id='RT3080' date='Due  19 Aug 2021' customer='Jensen Huang' price='£ 1,800.90' status={'pending'} />
+      <Invoice id='RT3080' date='Due  19 Aug 2021' customer='Jensen Huang' price='£ 1,800.90' status={'late'} />
+      <Invoice id='RT3080' date='Due  19 Aug 2021' customer='Jensen Huang' price='£ 1,800.90' status={'draft'} />
     </div>
   )
 }
